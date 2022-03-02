cdeb8bc0aa35270d28ff2ed4d821c5f27b621116
laross <laross@uark.edu>
2022-03-02 14:22:29 -0600

    stuff

--- SimpleWhisk/Form1.Designer.cs
+++ SimpleWhisk/Form1.Designer.cs
@@ -29,13 +29,46 @@ namespace SimpleWhisk
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Testbtn = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Testbtn
+            // 
+            this.Testbtn.Location = new System.Drawing.Point(311, 157);
+            this.Testbtn.Name = "Testbtn";
+            this.Testbtn.Size = new System.Drawing.Size(75, 23);
+            this.Testbtn.TabIndex = 0;
+            this.Testbtn.Text = "button1";
+            this.Testbtn.UseVisualStyleBackColor = true;
+            this.Testbtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(311, 199);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1235, 833);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Testbtn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Testbtn;
+        private System.Windows.Forms.Button button2;
     }
 }
 
